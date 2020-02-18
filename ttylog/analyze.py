2fa2ebc408e866e7453d02414816cfdd22a1f44f
Jack <cookjackc@gmail.com>
2020-02-18 11:18:50 -0800

    remove erroneous git tags

--- ttylog/analyze.py
+++ ttylog/analyze.py
@@ -353,19 +353,11 @@ if __name__ == "__main__":
             # If the initial prompt is encountered, it means that there was a
             # failed 'su' attempt, or an undetected exit from a different terminal prompt.
             # In these cases, revert to the initial prompt and home directory.
-
-<<<<<<< HEAD
             for prompt in known_prompts:
                 if prompt in line.casefold():
                     current_prompt = prompt
             user_prompt = current_prompt
             home_directory = '/home/' + user_prompt[0:user_prompt.index('@')]
-
-=======
-            if user_initial_prompt in line.casefold():
-                user_prompt = user_initial_prompt
-                home_directory = ttylog_sessions[current_session_id]['home_dir']
->>>>>>> 26a818a4b21df78d16249b993cb33b1cf10ddde9
 
             #If line is like 'googletest@intro:~$ ls;1554089474', 'google' is output of previous command
             start_of_prompt = line.casefold().find(user_prompt.casefold())
